add146c98e913e37a79aa0b8d00042ed965fd256
IsabelSilva-98 <maria.isabel.silva.98@gmail.com>
2023-05-05 16:56:26 +0100

    Novo repositorio

---
+++ Exercicio 3 - Aluguer de quartos (05-05-23)/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio_3___Aluguer_de_quartos__05_05_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+
+            Quarto[] vet = new Quarto[n];
+
+
+        }
+    }
+}
---
+++ Exercicio 3 - Aluguer de quartos (05-05-23)/Quarto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_3___Aluguer_de_quartos__05_05_23_
+{
+    internal class Quarto
+    {
+    }
+}
